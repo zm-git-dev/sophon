57596047bd49f5ff665bf7d483b8c3c957fbd4d5
fzyan <fzyan33@163.com>
2018-03-13 13:42:39 +0800

    HGT

---
+++ hGT/getHGTseq.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+use strict;
+
+my ($fa,$bed,$out)= @ARGV;
+die "Error with arguments!\nusage: $0 <Genome.fa> <HGT.bed> <OUT File>\n" if (@ARGV<3);
+open(FA,$fa)||die("error\n");
+open(BED,$bed)||die("error\n");
+open(OUT,">$out")||die("error\n");
+
+my %hash = ();
+my $id = "";
+while(<FA>){
+    chomp;
+    if($_ =~ />([^\s]+)/){
+	$id = $1;
+    }
+    else{
+	$hash{$id} = $_;
+    }
+}
+
+while(<BED>){
+    chomp();
+    my ($chr,$start,$end) = split(/\s+/,$_);
+    if(exists($hash{$chr})){
+	my $HGT = substr($hash{$chr},$start-1,$end-$start+1);
+	print OUT ">$chr|$start-$end\n$HGT\n";
+    }
+    else{
+	die("error with chr\n");
+    }
+}
+
+close FA;close BED;close OUT;
+exit;
---
+++ hGT/pipeline.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+# Get HGT sequences according to the genome and HGT annotation in bed format
+./getHGTseq.pl data/hg19/GRCh37.primary_assembly.genome.fa data/HGT-in-Human-Genome/material/data/iden40len20.bed iden40len20-hgt.fa
