e1c1430f4f4a5a9d5bbb0fc3605bdbd50eb4c7d9
fzyan <fzyan33@163.com>
2018-04-16 14:55:58 +0800

    region-non-mammal.pl

--- hGT/region-non-mammal.pl
+++ hGT/region-non-mammal.pl
@@ -1,52 +1,62 @@
 #!/usr/bin/perl -w
 use strict;
 
-#get regions conserved in non-mammal genomes, identities >= 40% && overlapped sequences >= 500bp && at least two species sharing this region
+#get regions conserved in non-mammal genomes, overlapped sequences >= 500bp && at least two non-mammal species sharing this region
 
 my (@files)= @ARGV;
 die "Error with arguments!\nusage: $0 <Filtered out files of non-mammal genomes> \n" if (@ARGV<1);
 
-my %hash = ();
+my %total = ();
 
 for my $file(@files){
     open(FILE,$file)||die("error\n");
+    my %hash = ();
+    print "$file\n";
     while(<FILE>){
-	if($_ !~ /Query/){
-	    my @arr = split(/\s+/,$_);
-	    my ($seq,$start,$end) = ($arr[3],$arr[4],$arr[5]);
-	    if($end < $start){
-		my $tmp = $start;
-		$start = $end;
-		$end = $tmp;
+	my @arr = split(/\s+/,$_);
+	my ($seq,$start,$end) = ($arr[0],$arr[6],$arr[7]);
+	if($end < $start){
+	    my $tmp = $start;
+	    $start = $end;
+	    $end = $tmp;
+	}
+	if(not exists($hash{$seq})){
+	    my @cov = ();
+	    for(my $i=0;$i<1000;$i++){
+		push(@cov,0);
 	    }
-	    if(not exists($hash{$seq})){
-		my @cov = ();
-		for(my $i=0;$i<1000;$i++){
-		    push(@cov,0);
-		}
-		for(my $i=$start-1;$i<$end;$i++){
-		    $cov[$i] += 1;
-		}
-		$hash{$seq} = \@cov;
+	    for(my $i=$start-1;$i<$end;$i++){
+		$cov[$i] = 1;
 	    }
-	    else{
-		for(my $i=$start-1;$i<$end;$i++){
-		    ${hash{$seq}}[$i] += 1;
-		}
+	    $hash{$seq} = \@cov;
+	}
+	else{
+	    for(my $i=$start-1;$i<$end;$i++){
+		${hash{$seq}}[$i] = 1;
 	    }
 	}
     }
     close FILE;
+    foreach my $key(keys %hash){
+	if(not exists $total{$key}){
+	    $total{$key} = $hash{$key};
+	}
+	else{
+	    for(my $i=0;$i<1000;$i++){
+		${total{$key}}[$i] = ${total{$key}}[$i] + $hash{$key}[$i];
+	    }
+	}
+    }
 }
 
-foreach my $key(keys %hash){
+foreach my $key(keys %total){
     my @start = ();
     my @end = ();
     for(my $i=0;$i<1000;$i++){
-	if(${hash{$key}}[$i] >= 2 && ($i ==0 || ${hash{$key}}[$i-1] < 2)){
+	if(${total{$key}}[$i] >= 2 && ($i ==0 || ${total{$key}}[$i-1] < 2)){
 	    push(@start,$i);
 	}
-	elsif(${hash{$key}}[$i] >= 2 && ($i == 999 || ${hash{$key}}[$i+1] < 2)){
+	elsif(${total{$key}}[$i] >= 2 && ($i == 999 || ${total{$key}}[$i+1] < 2)){
 	    push(@end,$i);
 	}
     }
