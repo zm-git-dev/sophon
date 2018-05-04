0acf52235f7aa6337cef6592ba87548a09c25716
fzyan <fzyan33@163.com>
2018-05-04 12:46:09 +0800

    blast filter pipeline

--- hGT/blast/run.sh
+++ hGT/blast/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-for obj in `cat non-mammal.txt`
+for obj in `cat non-mammal.id`
 do
-    grep -v "#" seg30M-$obj.blastn |awk '{if($4>=500){print $0}}' >non-mammal/$obj-500bp.out
+    echo $obj
+    #grep -v "#" seg30M-$obj.blastn |awk '{if($8-$7>=499 ||$8-$7<=-499 ){print $0}}' >non-mammal/$obj-500bp.out
 done
 
-
+for obj in `cat mammal.id`
+do
+    nohup cat seg30M-$obj* | grep -v "#" |awk '{if($8>$7) print $1"\t"$7"\t"$8; else print $1"\t"$8"\t"$7}' |sort -k1,1 -k2n,2 -k3n,3 |uniq >mammal/$obj-cov.txt &
+done
 
 exit
