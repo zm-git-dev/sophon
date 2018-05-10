5e9884c1b0e100cf2486976b926c940f7c1f40c6
fzyan <fzyan33@163.com>
2018-05-11 00:45:13 +0800

    lastz

---
+++ hGT/lastz/merge-nonmammal.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl -w
+use strict;
+
+#get regions conserved in non-mammal genomes, overlapped sequences >= 500bp && at least two non-mammal species sharing this region
+
+my ($index,$chr,$out,@files)= @ARGV;
+die "Error with arguments!\nusage: $0 <Reference chr_len.txt> <chromosome> <OUT File> <non-mammal bed files> \n" if (@ARGV<4);
+
+open(INDEX,$index)||die("error with opening $index\n");
+open(OUT,">$out")||die("error eith writing to $out\n");
+
+my %hash = ();
+print "build reference index...\n";
+
+my $chr_length = 0;
+while(<INDEX>){
+    chomp();
+    my ($seq_name,$seq_length) = split(/\s+/,$_);
+    if($seq_name eq $chr){
+	$chr_length = $seq_length;
+	print "chr length: $chr_length\n";
+	my @cov = ();
+	for(my $i=0;$i<$seq_length;$i++){
+	    push(@cov,0);
+	}
+	$hash{$chr} = \@cov;
+    }
+}
+print "build reference index complete!\n";
+
+foreach my $file(@files){
+    print "mapping $file...\n";
+    open(FILE,$file)||die("error with opening $file\n");
+    while(<FILE>){
+	chomp();
+	my @arr = split(/\s+/,$_);
+	my ($seq,$start,$end) = ($arr[0],$arr[1],$arr[2]);
+	if($seq eq $chr){
+	    for(my $i=$start-1;$i<=$end-1;$i++){
+		${hash{$seq}}[$i] += 1;
+	    }
+	}
+    }
+    close FILE;
+}
+print "construct conserved regions...\n";
+
+foreach my $key(keys %hash){
+    my @start = ();
+    my @end = ();
+    for(my $i=0;$i<$chr_length;$i++){
+	if(${hash{$key}}[$i] >= 2 && ($i ==0 || ${hash{$key}}[$i-1] < 2)){
+	    push(@start,$i);
+	}
+	elsif(${hash{$key}}[$i] >= 2 && ($i == $chr_length-1 || ${hash{$key}}[$i+1] < 2)){
+	    push(@end,$i);
+	}
+    }
+    for(my $i=0;$i<@start;$i++){
+	if($end[$i]-$start[$i] >= 499){
+	    print OUT "$key\t$start[$i]\t$end[$i]\n";
+	}
+    }
+}
+print "construct conserved regions complete!\n";
+
+close OUT;
+exit;
--- hGT/lastz/run.sh
+++ hGT/lastz/run.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-#if [ 1 == 2 ]
-#then
-#fi
+if [ 1 == 2 ]  ###code annotation start
+then
 
 for i in `cat 53species.id`
 do
-    echo $i
-    #nohup ./../axt2cov.pl cov/$i.cov /share/home/user/fzyan/hGT/data/data-wzhuang/vs$i/*.net.axt &
-    #nohup cat cov/$i.cov |sort -k1,1 -k2n,2 >cov/$i-sort.cov &
+    ./../axt2cov.pl cov/$i.cov /share/home/user/fzyan/hGT/data/data-wzhuang/vs$i/*.net.axt  #nohup 
+    cat cov/$i.cov |sort -k1,1 -k2n,2 >cov/$i-sort.cov #nohup 
+    rm cov/$i.cov
+    
+    '{if($9>=0.6){print $1"\t"$2"\t"$3}}' cov/$i-sort.cov >iden60/$i.bed
 done
 
+for i in `cat 12nonmammal.id`
+do
+    awk '{if($3-$2>=499){print $0}}' iden60/$i.bed >iden60/nonmammal-500bp/$i.bed
+done
 
+fi    ###code annotation end
+
+chr=(chr1 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr2 chr20 chr21 chr22 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chrX chrY)
+for i in ${chr[@]}
+do
+    echo $i
+    ./merge-nonmammal.pl chr_len.txt $i tmp-$i iden60/nonmammal-500bp/*bed
+done
+
+cat tmp-* |awk '{if($3-$2>=499){print $0}}' |sort -k1,1 -k2n,2 >iden60/nonmammal-500bp/merge-cov2-500bp.bed
