5ff08b8f9b6b0bfdc2612c29dd636cb5071fd3bc
fzyan <fzyan33@163.com>
2018-05-15 00:29:43 +0800

    lastz pipeline

--- hGT/lastz/run.sh
+++ hGT/lastz/run.sh
@@ -9,12 +9,12 @@ do
     cat cov/$i.cov |sort -k1,1 -k2n,2 >cov/$i-sort.cov #nohup 
     rm cov/$i.cov
     
-    '{if($9>=0.6){print $1"\t"$2"\t"$3}}' cov/$i-sort.cov >iden60/$i.bed
+    awk '{if($9>=0.6){print $1"\t"$2"\t"$3}}' cov/$i-sort.cov >iden60/$i.bed
 done
 
 for i in `cat 12nonmammal.id`
 do
-    awk '{if($3-$2>=999){print $0}}' iden60/$i.bed >iden60/nonmammal-1kbp/$i.bed
+    #awk '{if($3-$2>=999){print $0}}' iden60/$i.bed >iden60/nonmammal-1kbp/$i.bed
     cp iden60/$i.bed iden60/nonmammal/$i.bed
 done
 
@@ -22,10 +22,10 @@ chr=(chr1 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr2 chr20
 for i in ${chr[@]}
 do
     echo $i
-    ./merge-nonmammal.pl chr_len.txt $i tmp-$i iden60/nonmammal-1kbp/*bed
+    ./merge-nonmammal.pl chr_len.txt $i tmp-$i iden60/nonmammal/*bed
 done
 
-cat tmp-* |sort -k1,1 -k2n,2 >iden60/nonmammal-1kbp/merge-cov2-1kbp.bed
+cat tmp-* |sort -k1,1 -k2n,2 >iden60/nonmammal/merge-cov2-1kbp.bed
 rm tmp-*
 
 for i in ${chr[@]}
@@ -40,20 +40,12 @@ for id in `cat 41mammal.id`
 do
     ./../src/merge-mammal.pl iden60/$id.bed iden60/mammal/$id-merged.txt
     
-    nohup ./../src/screenHGT.pl iden60/nonmammal-1kbp/merge-cov2-1kbp.bed tmp-$id 0.4 iden60/mammal/$id-merged.txt >qq-$id &
+    nohup ./../src/screenHGT.pl iden60/nonmammal/merge-cov2-1kbp.bed tmp-$id 0.4 iden60/mammal/$id-merged.txt &
 done
 
 fi    ###code annotation end
 
-./summaryMammal.pl iden60/nonmammal-1kbp/merge-cov2-1kbp.bed  screenHGT.out tmp-*
-rm tmp* qq*
+./summaryMammal.pl iden60/nonmammal/merge-cov2-1kbp.bed  iden60/nonmammal/screenHGT.out tmp-*
+rm tmp* 
 
-
-for id in `cat 41mammal.id`
-do
-    nohup ./../src/screenHGT.pl iden60/nonmammal/merge-cov2-1kbp.bed tmp-$id 0.4 iden60/mammal/$id-merged.txt >qq-$id &
-done
-
-./summaryMammal.pl iden60/nonmammal/merge-cov2-1kbp.bed screenHGT2.out tmp-*
-rm tmp* qq*
 
