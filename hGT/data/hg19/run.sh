4272d69ac60a7f5d3934125fb0a4acf46b0fa737
fzyan <fzyan33@163.com>
2018-05-26 00:18:25 +0800

    rmsk

--- hGT/data/hg19/run.sh
+++ hGT/data/hg19/run.sh
@@ -14,3 +14,12 @@ others_num=$(egrep -v "antisense|lincRNA|polymorphic_pseudogene|protein_coding|p
 others_len=$(egrep -v "antisense|lincRNA|polymorphic_pseudogene|protein_coding|pseudogene" gencode.v19.geneinfo.bed |awk '{print $3-$2+1}' |awk '{sum+=$1} END {print sum}')
 echo -e "others\t$others_num\t$others_len"
 
+for i in `cat rmsk/type.txt`
+do
+    grep "\s$i$" rmsk.bed >rmsk/$i.bed
+    cat rmsk/$i.bed |sort -k1,1 -k2n,2 >rmsk/$i.bed.tmp
+    ./mergeBed.pl rmsk/$i.bed.tmp rmsk/$i-merge.bed
+    rm rmsk/$i.bed.tmp
+done
+
+cat rmsk/*-merge.bed |sort -k1,1 -k2n,2 >rmsk-merge.bed
