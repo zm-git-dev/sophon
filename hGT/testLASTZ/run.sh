09ea7ccaf470209227f7da62d09dd67f32b8e6d3
fzyan <fzyan33@163.com>
2018-07-07 14:01:38 +0800

    testLASTZ

--- hGT/testLASTZ/run.sh
+++ hGT/testLASTZ/run.sh
@@ -27,9 +27,9 @@ do
 	mv iden60/$tar-$id-merge.bed iden60/$tar-$id.bed
     done
 
-    for i in `cat 12nonmammal.id`
+    for id in `cat 12nonmammal.id`
     do
-        cp iden60/$tar-$i.bed iden60/nonmammal/
+        cp iden60/$tar-$id.bed iden60/nonmammal/
     done
 
     for id in `cat 10mammal.id`
@@ -55,14 +55,61 @@ awk '{if($4<=2) print $1"-"$2"-"$3}' screenHGT-chr22.out |awk -F '-' '{print $1"
 ~/hGT/src/getHGTseq.pl ~/hGT/data/hg19/hg19-UCSC-uc.fa screenHGT-2mammals-chr22.bed screenHGT-2mammals-chr22.fa
 ~/hGT/src/getHGTseq.pl ~/hGT/data/hg19/hg19-UCSC-uc.fa screenHGT-2mammals-350seg-merge.bed screenHGT-2mammals-350seg-merge.fa
 
+~/hGT/src/biodiff.pl ~/hGT/data/hg19/rmsk-merge.bed screenHGT-2mammals-chr22.bed 68hgt2repeat.txt
+~/hGT/src/biodiff.pl ~/hGT/data/hg19/rmsk-merge.bed screenHGT-2mammals-350seg-merge.bed 32hgt2repeat.txt
+
 
 arr=(screenHGT-2mammals-350seg-merge screenHGT-2mammals-chr22)
-for id in ${cov[@]}
+for id in ${arr[@]}
 do
-    blastn -task blastn -query $id.fa -db ~/hGT/db/hg19 -out $id-hg19.blastn -evalue 1e-3 -num_threads 16 -outfmt 7 -word_size 9
+    nohup blastn -task blastn -query $id.fa -db ~/hGT/db/hg19 -out $id-hg19.blastn -evalue 1e-3 -num_threads 16 -outfmt 7 -word_size 9 &
+done
+
+mkdir hit-350seg hit-chr22
+~/hGT/src/filterHits.pl screenHGT-2mammals-350seg-merge-hg19.blastn hit-350seg
+~/hGT/src/filterHits.pl screenHGT-2mammals-chr22-hg19.blastn hit-chr22
+
+
+mkdir hit-350seg/iden90 hit-chr22/iden90
+for tar in ${target[@]}
+do
+    for i in hit-$tar/*.txt
+    do
+	echo $i
+	pre=${i%.txt}
+	suf=${pre#hit-$tar/}
+	awk '{if($4>=90) print $0}' $i >hit-$tar/iden90/$suf.txt
+	
+	cat hit-$tar/iden90/$suf.txt |sort -k1,1 -k2n,2 >mm
+	~/hGT/src/mergeBed.pl mm hit-$tar/iden90/$suf.merge.txt
+	rm mm
+    done
 done
 
 fi   ###code annotation end
 
-nohup ~/hGT/src/biodiff.pl ~/hGT/data/hg19/rmsk-merge.bed screenHGT-2mammals-chr22.bed 68hgt2repeat.txt &
-nohup ~/hGT/src/biodiff.pl ~/hGT/data/hg19/rmsk-merge.bed screenHGT-2mammals-350seg-merge.bed 32hgt2repeat.txt &
+for id in `grep ">" screenHGT-2mammals-350seg-merge.fa |tr -d ">"`
+do
+    org_num=$(cat hit-350seg/iden90/$id.txt |wc -l)
+    num=$(cat hit-350seg/iden90/$id.merge.txt |wc -l)
+    avg_len=$(awk '{print $3-$2+1}' hit-350seg/iden90/$id.merge.txt |awk '{sum+=$1} END {print "",sum/NR}')
+    cov_len=$(awk '{print $3-$2+1}' hit-350seg/iden90/$id.merge.txt |awk '{sum+=$1} END {print "",sum}')
+    echo -e "$id\t$num\t$avg_len\t$cov_len\t$org_num" >>mm
+done
+
+echo -e "id\thit_num\tavg_len\tcov_len" >>summaryHit-iden90-350seg.txt
+awk '{if($5==0) print $1"\t0\t0\t0\t0"; else print $1"\t"$2"\t"$3"\t"$4}' mm | sort -rnk 2 >>summaryHit-iden90-350seg.txt
+rm mm
+
+for id in `grep ">" screenHGT-2mammals-chr22.fa |tr -d ">"`
+do
+    org_num=$(cat hit-chr22/iden90/$id.txt |wc -l)
+    num=$(cat hit-chr22/iden90/$id.merge.txt |wc -l)
+    avg_len=$(awk '{print $3-$2+1}' hit-chr22/iden90/$id.merge.txt |awk '{sum+=$1} END {print "",sum/NR}')
+    cov_len=$(awk '{print $3-$2+1}' hit-chr22/iden90/$id.merge.txt |awk '{sum+=$1} END {print "",sum}')
+    echo -e "$id\t$num\t$avg_len\t$cov_len\t$org_num" >>mm
+done
+
+echo -e "id\thit_num\tavg_len\tcov_len" >>summaryHit-iden90-chr22.txt
+awk '{if($5==0) print $1"\t0\t0\t0\t0"; else print $1"\t"$2"\t"$3"\t"$4}' mm | sort -rnk 2 >>summaryHit-iden90-chr22.txt
+rm mm
