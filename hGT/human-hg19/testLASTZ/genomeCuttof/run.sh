e04b6450ab28577f00bc3739a9e7f4ab764c21eb
fzyan <fzyan33@163.com>
2018-07-23 14:19:38 +0800

    choose parameters for cutting genome

--- hGT/human-hg19/testLASTZ/genomeCuttof/run.sh
+++ hGT/human-hg19/testLASTZ/genomeCuttof/run.sh
@@ -11,14 +11,13 @@ do
 	#nohup ~/hGT/src/segment.pl ../chr22.fa $i $j chr22-seg$i-step$j.fa &
 	for k in ${kmer[@]}
 	do
-	    #nohup ~/hGT/src/kmer.pl chr22-seg$i-step$j.fa $k >chr22-seg$i-step$j-$k"mer".txt &
-	    #nohup ~/hGT/src/compareKmer.pl ~/hGT/human-hg19/kmer/hg19-$k"mer".txt chr22-seg$i-step$j-$k"mer".txt chr22-seg$i-step$j-$k"mer"-distance.txt &
-	    #num=$(grep -v region chr22-seg$i-step$j-$k"mer"-distance.txt |wc -l)
-	    #top=`expr $num / 100`
-	    #grep -v region chr22-seg$i-step$j-$k"mer"-distance.txt |sort -rnk 2 |head -$top |awk '{print $1}' >chr22-seg$i-step$j-$k"mer"-distance-pass.info
-	    #awk -F '-' '{print $1"\t"$2"\t"$3}' chr22-seg$i-step$j-$k"mer"-distance-pass.info |sort -k1,1 -k2n,2 >chr22-seg$i-step$j-$k"mer"-distance-pass.bed
-	    #overlap=$(./biodiff-500bp.pl chr22-seg$i-step$j-$k"mer"-distance-pass.bed ../screenHGT-chr22.bed |awk '{if($5>=500) print $1$2$3}' |sort |uniq |wc -l)
-	    #echo -e "$i\t$j\t$k\t$overlap"
+	    nohup ~/hGT/src/kmer.pl chr22-seg$i-step$j.fa $k >chr22-seg$i-step$j-$k"mer".txt &
+	    nohup ~/hGT/src/compareKmer.pl ~/hGT/human-hg19/kmer/hg19-$k"mer".txt chr22-seg$i-step$j-$k"mer".txt chr22-seg$i-step$j-$k"mer"-distance.txt &
+	    num=$(grep -v region chr22-seg$i-step$j-$k"mer"-distance.txt |wc -l)
+	    top=`expr $num / 100`
+	    grep -v region chr22-seg$i-step$j-$k"mer"-distance.txt |sort -rnk 2 |head -$top |awk '{print $1}' >chr22-seg$i-step$j-$k"mer"-distance-pass.info
+	    awk -F '-' '{print $1"\t"$2"\t"$3}' chr22-seg$i-step$j-$k"mer"-distance-pass.info |sort -k1,1 -k2n,2 >chr22-seg$i-step$j-$k"mer"-distance-pass.bed
+	    overlap=$(./biodiff-500bp.pl chr22-seg$i-step$j-$k"mer"-distance-pass.bed ../screenHGT-chr22.bed |awk '{if($5>=500) print $1$2$3}' |sort |uniq |wc -l)
 	    ~/hGT/src/getHGTseq.pl ../chr22.fa chr22-seg$i-step$j-$k"mer"-distance-pass.bed chr22-seg$i-step$j-$k"mer"-distance-pass.fa 
 	done
     done
